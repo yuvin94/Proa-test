32ea4d86adba51946f2fd493749eb60dd92aaedb
yuvin94 <100668157+yuvin94@users.noreply.github.com>
2025-05-15 18:50:41 +1000

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,19 @@
-import requests
+# import requests
 
-def test_healthz():
-    r = requests.get("http://localhost:5000/healthz")
-    assert r.status_code == 200
+# def test_healthz():
+#     r = requests.get("http://localhost:5000/healthz")
+#     assert r.status_code == 200
+
+
+import pytest
+from Proa-test/app/app import app  # Replace with the name of your Python file without `.py`
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_healthz(client):
+    response = client.get("/healthz")
+    assert response.status_code == 200
+    assert response.data.decode() == "OK"
