3527477e33c75f8fd574df7e0737a9bcd4cca37e
yuvin94 <100668157+yuvin94@users.noreply.github.com>
2025-05-19 11:13:25 +1000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,19 +1,30 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+
+# @app.route("/")
+# def hello()
+#     """Return greeting."""
+#     return "Hello, world! Testing with Brad"
+
+
+# @app.route("/healthz")
+# def health():
+#     """Health check endpoint."""
+#     return "OK", 200
+
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
+
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello()
-    """Return greeting."""
-    return "Hello, world! Testing with Brad"
-
-
-@app.route("/healthz")
-def health():
-    """Health check endpoint."""
-    return "OK", 200
-
+def hello():
+    return "Hello, world!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
